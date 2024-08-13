8127e181f798dc0f1901ab3d65b1c17f0ae61c55
Gabrielle Rodrigues <silva111gabi16@gmail.com>
2024-08-13 18:23:55 -0300

    js
    exemplo 2 pratica

---
+++ Fundamentos da Programação Web/JAVASCRIPT/exemplo2.js
@@ -0,0 +1,10 @@
+let sexo = 'F'
+if (sexo == 'F'){
+    console.log('Você é do sexo feminino.')
+}
+else if (sexo == 'M'){
+    console.log('Você é do sexo masculino.')
+}
+else {
+    console.log('Sexo inválido')
+}
