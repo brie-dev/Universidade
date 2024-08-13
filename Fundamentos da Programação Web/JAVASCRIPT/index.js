69666265fbdb72ad3342014620eae6d06a17ad8e
Gabrielle Rodrigues <silva111gabi16@gmail.com>
2024-08-13 17:55:32 -0300

    js

--- Fundamentos da Programação Web/JAVASCRIPT/index.js
+++ Fundamentos da Programação Web/JAVASCRIPT/index.js
@@ -5,3 +5,36 @@ console.log(nome, altura)
 let a = 5;
 let b = 10;
 console.log(`O resultado da operação é ${a+b}`)
+
+// let pecaPreco = 10;
+// let pecas = prompt('Qual a quantidade de peças encomendadas: ')
+
+// if (pecas>0){
+//     let total = pecaPreco*pecas;
+//     console.log(total);
+// }
+
+let nomeSenha = 'Gabrielle'
+if (nomeSenha == 'Gabrielle' || nomeSenha == 'José'){
+    console.log('liberado')
+}
+else {
+    console.log('negado')
+}
+
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+let num = 0;
+while(num <= 90){
+    console.log(num)
+    num+=10;
+}
+
+let numero = 0;
+do{
+    console.log(num)
+    num+=10;
+}
+while(num <= 90);
